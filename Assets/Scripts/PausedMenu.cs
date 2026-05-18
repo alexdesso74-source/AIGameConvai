e4b526963e30a6f35076c174c337213bd9df1c28
alexdesso74-source <alexdesso74@gmail.com>
2026-05-18 16:07:54 +0100

    Pause Menu

---
+++ Assets/Scripts/PausedMenu.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            Time.timeScale = pauseMenu.activeSelf ? 0 : 1;
+        }    
+    }
+    public void ResumeButton(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
